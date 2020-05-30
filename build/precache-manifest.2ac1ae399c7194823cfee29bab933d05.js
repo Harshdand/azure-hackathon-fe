7778cbba5823611aec6cac712d9a48f877fc567c
harshdand <harshdand2@gmail.com>
2020-05-31 02:14:55 +0530

    initial build

---
+++ build/precache-manifest.2ac1ae399c7194823cfee29bab933d05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df90d54e74820c59f393b80abb974486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0504ae9c3b9c5e594d6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a3928141c4a698625206",
+    "url": "/static/css/main.6b8e6dd2.chunk.css"
+  },
+  {
+    "revision": "a0504ae9c3b9c5e594d6",
+    "url": "/static/js/2.9c2d5e72.chunk.js"
+  },
+  {
+    "revision": "02b3897220fa0ff5376734687e85d84a",
+    "url": "/static/js/2.9c2d5e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3928141c4a698625206",
+    "url": "/static/js/main.72a40d94.chunk.js"
+  },
+  {
+    "revision": "db154035dc3a964deeab",
+    "url": "/static/js/runtime-main.08ee8178.js"
+  }
+]);
